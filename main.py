fb3c3000871f787758f042970a18e474252171ce
Amber Orrange <orrangeamber@gmail.com>
2021-12-07 17:58:26 -0600

    retrieved html

---
+++ main.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+url = "https://apps.irs.gov/app/picklist/list/priorFormPublication.html"
+page = urlopen(url)
+html = page.read().decode("utf-8")
+
+soup = BeautifulSoup(html, "html.parser")
+
+
+#the top steps get me the html
+#now I need to parse through the html and fill out the form with the given form name
+#click submit and receive results
+# on the results page I need to sort by date, earliest to latest and make s
+#names are an exact match....how to deal with multiple pages of results
+
+
+
+print(soup.get_text())
