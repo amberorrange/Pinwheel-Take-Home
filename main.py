b07a64723fc8451270b87d321678841a453f5d5a
Amber Orrange <orrangeamber@gmail.com>
2021-12-08 17:36:02 -0600

    url change

--- main.py
+++ main.py
@@ -4,20 +4,54 @@ import mechanicalsoup
 
 
 browser = mechanicalsoup.Browser()
-url = "https://apps.irs.gov/app/picklist/list/priorFormPublication.html"
-page = browser.get(url)
-page_html = page.soup
+# url = "https://apps.irs.gov/app/picklist/list/priorFormPublication.html"
+# page = browser.get(url)
+# page_html = page.soup
+
+
+beginning_url = "https://apps.irs.gov/app/picklist/list/priorFormPublication.html;jsessionid=rhBpcYp77FqlnEus-PDcS79Y.20?value=" 
+end_url = "&criteria=formNumber&submitSearch=Find"
+
+form_list = ["Form 1095-C"]
+
+for form in form_list:
+    joined_name = form.replace(" ", "+")
+    print(joined_name)
+
+    url = beginning_url + joined_name + end_url
+
+    page = browser.get(url)
+    page_html = page.soup
+
+    print(page_html)
+
+
+
+
+
 
 
 #get the form
-form = page_html.select("form")[0]
+# form = page_html.select("form")
+
+# search_box = page_html.select("input", id="searchFor")[0]["value"] = "Form 1095-C"
+
+# results_page = (browser.submit((form, page.url)))
+
+
+
+# submit_btn = page_html.select("input", id="searchFor")[1]
+
+# print(search_box)
+# print(submit_btn)
+
+
 
 
 
 # form.select("input")[3]["value"] = "zeus"
 
 
-print(form)
 
 # print(form)
 # form.select("input")[0]["value"] = "Form 1095-C"
