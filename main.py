17e3e08242b9cbda999f2f770a699979729be513
Amber Orrange <orrangeamber@gmail.com>
2021-12-10 14:40:42 -0600

    finished part 1

--- main.py
+++ main.py
@@ -3,11 +3,8 @@ from urllib.request import urlopen
 import mechanicalsoup
 import math
 
-# browser = mechanicalsoup.Browser()
 
-#these do not change
-BEGINNING_URL = "https://apps.irs.gov/app/picklist/list/priorFormPublication.html?sortColumn=currentYearRevDate&indexOfFirstRow=0&value=" 
-END_URL = "&criteria=formNumber&resultsPerPage=200&isDescending=false"
+# browser = mechanicalsoup.Browser()
 
 form_list = ["Form 1095-C", "Form W-2"]
 
@@ -28,68 +25,53 @@ def find_num_of_results(results_html):
         return num_of_results
 
 
-def initial_search(form_name):
-    """Returns the number of page results after searching for a form"""
-    joined_name = form_name.replace(" ", "+")
-    url = BEGINNING_URL + joined_name + END_URL
-    page = urlopen(url)
+def search_webpage(form_name, starting_point):
+    """Retrieves and returns the HTML of a search results page."""
+
+    beginning_url = f"https://apps.irs.gov/app/picklist/list/priorFormPublication.html?sortColumn=currentYearRevDate&indexOfFirstRow={starting_point}&value=" 
+    end_url = "&criteria=formNumber&resultsPerPage=200&isDescending=false"
+
+    form_name = form_name.replace(" ", "")
+    url = beginning_url + form_name + end_url
+    page = urlopen(url) 
     html_bytes = page.read()
     page_html = html_bytes.decode("utf-8")
     parsed_html = BeautifulSoup(page_html, "html.parser")
 
-    num_of_results = find_num_of_results(parsed_html)
-    page_results = find_num_of_page_results(num_of_results)
-
-    return page_results
-
-
+    return parsed_html
 
 def parse_forms_list(forms_to_add, results_lst, form_name):
-    """Takes in the html all results, parses it, 
+    """Takes in the html of the form results, parses it, 
     and adds it to an array. Returns the array"""
 
     for item in forms_to_add:
             product_name = item.find("a").get_text().strip()
             title = item.find("td", class_="MiddleCellSpacer").get_text().strip()
             year = item.find("td", class_="EndCellSpacer").get_text().strip()
-            # print(product_name, title, year)
 
+            #ensures that you only return exact matches
             if product_name == form_name:
                 results_lst.append([product_name, title, year])
 
     return results_lst
 
 
-
-
-
-
-
-
-        
-
 def get_search_results(forms_to_search):
 
     final_results = []
 
     for form in forms_to_search:
-        num_page_results = initial_search(form)
+
+        results_html = search_webpage(form, 0)
+        num_total_results = find_num_of_results(results_html)
+        num_page_results = find_num_of_page_results(num_total_results)
+
         results_lst = []
 
         for page_number in range(num_page_results):
             ind_of_first_row = (200 * page_number)
 
-            # print(form, page_number, ind_of_first_row)
-            first_part_url = f"https://apps.irs.gov/app/picklist/list/priorFormPublication.html?sortColumn=currentYearRevDate&indexOfFirstRow={ind_of_first_row}&value=" 
-
-            url = first_part_url + form + END_URL
-            url = url.replace(" ","")
-            print(url)
-
-            page = urlopen(url)
-            html_bytes = page.read()
-            page_html = html_bytes.decode("utf-8")
-            parsed_html = BeautifulSoup(page_html, "html.parser")
+            parsed_html = search_webpage(form, ind_of_first_row)
 
             evens = parsed_html.find_all("tr", class_="even")
             odds = parsed_html.find_all("tr", class_="odd")
@@ -97,19 +79,13 @@ def get_search_results(forms_to_search):
             results_lst = parse_forms_list(evens, results_lst, form)
             results_lst = parse_forms_list(odds, results_lst, form)
 
-
+            #add the years from the results into a list to find min and max
             years = []
-
             for item in results_lst:
                 years.append(int(item[2]))
 
-        # print("years: ", years)
-        
         minimum = min(years)
         maximum = max(years)
-
-        # print(minimum, maximum)
-
 
         form_dict = {"form_number": results_lst[0][0], "form_title": results_lst[0][1],
                     "min_year": minimum, "max_year": maximum}
@@ -117,7 +93,6 @@ def get_search_results(forms_to_search):
         final_results.append(form_dict)
 
     print(final_results)
-
 
 
 get_search_results(form_list)
